d809998145f7bfb64263fb4026f82f4747d373de
simoh8 <simomutu8@gmail>
2025-09-01 23:04:03 +0300

    cleaned the code

--- app.py
+++ app.py
@@ -422,4 +422,5 @@ def static_files(path):
     return send_from_directory('.', path)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
